75dabda386faec3c31ab078756392909d69c4165
inc0gnit0 <iinc0gnit0@pm.me>
2019-08-17 17:06:34 -0500

    Update install.py

--- install.py
+++ install.py
@@ -1,12 +1,13 @@
 import os
+import time
 if os.getuid() != 0:
 	exit("\033[31m" + "Please run this as root, or use sudo!" + "\033[0m")
 os.system("apt-get update")
 os.system("apt-get install python3")
 os.system("apt-get install python3-pip")
 os.system("pip3 install pybase64")
-os.system("pip3 install pyarmor")
-os.system("pip3 install sqlite3")
 print("\n")
-print("\033[31m" + "Please run the command: python3 cryptz.py" + "\033[0m")
+print("\033[31m" + "To run the scripts use: python3 cryptz.py" + "\033[0m")
+sleep(3)
 print("\n")
+os.system("python3 cryptz.py")
